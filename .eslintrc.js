20bf9fc8bfc6f76052843074952451999450e0c4
Stefan Rom <srom@samhammer.de>
2018-07-24 11:13:03 +0200

    SSP-2029 Renamed project for more consistency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,29 @@
 'use strict';
 
 module.exports = {
-	extends: 'ckeditor5',
-	rules: {
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'indent': [
-			'error',
-			4
-		],
-		'array-bracket-spacing': [
-			'error',
-			'never'
-		],
-		'padded-blocks': [
-			'error',
-			{
-				'classes': 'always'
-			}
-		]
-	}
+    extends: 'ckeditor5',
+    rules: {
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'indent': [
+            'error',
+            4
+        ],
+        'array-bracket-spacing': [
+            'error',
+            'never'
+        ],
+        'padded-blocks': [
+            'error',
+            {
+                'classes': 'always'
+            }
+        ],
+        'space-in-parens': [
+            'error',
+            'never'
+        ]
+    }
 };
--- src/internalLink.js
+++ src/internalLink.js
@@ -1,13 +1,13 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 
-export default class InlineLink extends Plugin {
+export default class internalLink extends Plugin {
 
     static get requires() {
         return [];
     }
 
     static get pluginName() {
-        return 'InlineLink';
+        return 'internalLink';
     }
 
 }
