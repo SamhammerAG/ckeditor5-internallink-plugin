284f4cc48185a2290cc903fcdc32a76297fb162a
Stefan Rom <srom@samhammer.de>
2018-07-26 09:51:57 +0200

    SSP-2029 We dont support collapsed slections at the moment - removed this code for better maintainability

--- src/InternalLinkUi.js
+++ src/InternalLinkUi.js
@@ -501,25 +501,21 @@ export default class InternalLinkUi extends Plugin {
     getSelectedLinkElement() {
         const selection = this.editor.editing.view.document.selection;
 
-        if (selection.isCollapsed) {
-            return this.findLinkElementAncestor(selection.getFirstPosition());
+        // The range for fully selected link is usually anchored in adjacent text nodes.
+        // Trim it to get closer to the actual link element.
+        const range = selection.getFirstRange().getTrimmed();
+        const startLink = this.findLinkElementAncestor(range.start);
+        const endLink = this.findLinkElementAncestor(range.end);
+
+        if (!startLink || startLink != endLink) {
+            return null;
+        }
+
+        // Check if the link element is fully selected.
+        if (Range.createIn(startLink).getTrimmed().isEqual(range)) {
+            return startLink;
         } else {
-            // The range for fully selected link is usually anchored in adjacent text nodes.
-            // Trim it to get closer to the actual link element.
-            const range = selection.getFirstRange().getTrimmed();
-            const startLink = this.findLinkElementAncestor(range.start);
-            const endLink = this.findLinkElementAncestor(range.end);
-
-            if (!startLink || startLink != endLink) {
-                return null;
-            }
-
-            // Check if the link element is fully selected.
-            if (Range.createIn(startLink).getTrimmed().isEqual(range)) {
-                return startLink;
-            } else {
-                return null;
-            }
+            return null;
         }
     }
 
