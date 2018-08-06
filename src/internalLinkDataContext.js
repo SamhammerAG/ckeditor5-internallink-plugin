27f34b6b1e1553fd9d147f41da76e54279a6fff5
Stefan Rom <srom@samhammer.de>
2018-08-06 09:55:29 +0200

    SSP-2112 Added data context

---
+++ src/internalLinkDataContext.js
@@ -0,0 +1,85 @@
+/**
+ * @module InternalLink/InternalLinkDataContext
+ */
+
+// import axios from 'axios';
+
+import { replacePlaceholderInUrl } from './utils';
+
+import {
+    CONFIG_TEST_MODE,
+    CONFIG_BASE_URL,
+    CONFIG_AUTOCOMPLETE_URL,
+    CONFIG_TITLE_URL,
+    URL_PLACEHOLDER_ID,
+    URL_PLACEHOLDER_SEARCH_TERM
+} from './constants';
+
+/**
+ * This is used to call a web service for finding
+ * items and getting the item name by id.
+ */
+export default class InternalLinkDataContext {
+
+    constructor(editor) {
+        this.editor = editor;
+    }
+
+    /**
+     * Gets the autocomplete suggestions
+     * @param {string} searchTerm The term that is entered into our searchbox
+     */
+    autocomplete(searchTerm) {
+        const isTestMode = this.editor.config.get(CONFIG_TEST_MODE);
+        const templateUrl = this.editor.config.get(CONFIG_BASE_URL) + this.editor.config.get(CONFIG_AUTOCOMPLETE_URL);
+        const autocompleteUrl = replacePlaceholderInUrl(templateUrl, URL_PLACEHOLDER_SEARCH_TERM, searchTerm);
+
+        if (isTestMode) {
+            return this.getAutocompleteTestData(searchTerm);
+        }
+
+        console.log(autocompleteUrl); // eslint-disable-line
+    }
+
+    /**
+     * Loads the title of an item
+     * @param {string} itemId The id of an item you want to load
+     */
+    getTitleById(itemId) {
+        const isTestMode = this.editor.config.get(CONFIG_TEST_MODE);
+        const templateUrl = this.editor.config.get(CONFIG_BASE_URL) + this.editor.config.get(CONFIG_TITLE_URL);
+        const titleUrl = replacePlaceholderInUrl(templateUrl, URL_PLACEHOLDER_ID, itemId);
+
+        if (isTestMode) {
+            return this.getTitleTestData(itemId);
+        }
+
+        console.log(titleUrl); // eslint-disable-line
+    }
+
+    /**
+     * Gets the autocomplete test data
+     * @param {string} searchTerm The term that is entered into our searchbox
+     */
+    getAutocompleteTestData(searchTerm) {
+        return [
+            { label: 'SearchTerm: ' + searchTerm, value: '1' },
+            { label: 'Vuejs', value: '1001' },
+            { label: 'Javascript', value: '500' }];
+    }
+
+    /**
+     * Gets the title test data
+     * @param {string} itemId The id of an item you want to load
+     */
+    getTitleTestData(itemId) {
+        if (itemId == '500') {
+            return 'Javascript';
+        } else if (itemId == '1001') {
+            return 'Vuejs';
+        } else {
+            return 'Item: ' + itemId;
+        }
+    }
+
+}
